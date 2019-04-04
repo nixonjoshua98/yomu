178c01d014e68a4470db28fb23f976611e4b0e25
nixonjoshua98 <nixonjoshua98@gmail.com>
2019-04-04 16:45:28 +0100

    Update serverRequest.py

--- Scrapper/functions/serverRequest.py
+++ Scrapper/functions/serverRequest.py
@@ -2,6 +2,8 @@ import requests
 
 def sendRequest(url):
     headers = requests.utils.default_headers()
+
+    headers["user-agent"] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36"
     
     try:
         page = requests.get(url, stream = True, timeout = 5, headers = headers)

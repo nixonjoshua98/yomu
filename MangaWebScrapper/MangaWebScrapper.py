074376eea9c9fbb1bdba75571fadb6f9e8add4f4
Joshua Nixon <nixonjoshua98@gmail.com>
2019-05-04 20:29:55 +0100

    Updated

---
+++ MangaWebScrapper/MangaWebScrapper.py
@@ -0,0 +1,35 @@
+
+"""
+Author: Joshua Nixon
+Email: nixonjoshua98@gmail.com
+Py Version: 3.7.2
+Sublime Text 3
+"""
+
+def get_old_database():
+	import sqlite3
+	from database.database_queries import manga_insert_row
+
+
+	with sqlite3.connect(r"C:\Users\Joshua\Documents\Repos\ManganeloWebScrapper\Scrapper\data\DB.DB") as db:
+		cursor = db.cursor()
+		cursor.execute("SELECT * FROM COMICS")
+		rows = cursor.fetchall()
+
+
+	for i, r in enumerate(rows):
+		manga_insert_row(title = r[1],
+			menu_url = r[2],
+			chapters_read = r[4],
+			status = r[5],
+			)
+
+if __name__ == "__main__":
+	import user_interface.windows as windows
+	from database.database_alchemy import Database
+
+	Database.create()
+
+	windows.Application().mainloop()
+
+	

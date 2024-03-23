390f5bf825bcc5322946a28665fae1f3b0c8a37a
Joshua <nixonjoshua98@gmail.com>
2024-03-23 16:03:12 +0000

    Fixed PS

--- Run.ps1
+++ Run.ps1
@@ -2,10 +2,12 @@ $currentDirectory = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
 $pythonRunScript = Join-Path -Path $currentDirectory -ChildPath '\run.py'
 
+$pythonPath = Join-Path -Path $currentDirectory -ChildPath '\venv\scripts\python.exe'
+
 $activateScriptPath = Join-Path -Path $currentDirectory -ChildPath '\venv\scripts\activate.ps1'
 
 Invoke-Expression -Command $activateScriptPath
 
-python $pythonRunScript
+Invoke-Expression -Command "$pythonPath $pythonRunScript"
 
 deactivate
